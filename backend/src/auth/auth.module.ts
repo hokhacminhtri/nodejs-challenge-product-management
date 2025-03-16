f81e340ab512b2ffbc17112b5e5da6a4b940393c
Minh Trí <hokhacminhtri@gmail.com>
2025-03-17 02:23:13 +0700

    Update backend env variables & Update seed.ts file

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import * as dotenv from 'dotenv';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
+
+dotenv.config();
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'secretKey', // Use environment variable in production
-      signOptions: { expiresIn: '1h' },
+      secret: process.env.JWT_SECRET || 'defaultSecretKey', // Use environment variable in production
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1h' },
     }),
   ],
   providers: [JwtStrategy, AuthService],
